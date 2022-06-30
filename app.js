04ace6444bef55fbebac50f93922043eec2204a0
Rahul Shingala <rahulshingala111@gmail.com>
2022-06-30 09:31:49 +0530

    created basic view(res.render)

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')})
+    res.render('home');
+})
 
 app.listen(3000,()=>{
     console.log("Server Started on PORT 3000.......");
