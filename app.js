f55f82a510f8c13676e0aea7adc25ba0a248388e
Rahul Shingala <rahulshingala111@gmail.com>
2022-06-30 09:39:18 +0530

    created Schema | set up mongoose fie files

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-
-
 
 app.get('/', (req, res) => {
     res.render('home');
