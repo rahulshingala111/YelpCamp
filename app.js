79b9a26b282f06f684c21a2f7724805f7af615b0
Rahul Shingala <rahulshingala111@gmail.com>
2022-06-30 09:50:16 +0530

    database connected to Mongoos

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
 const path = require('path');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",()=>{
+    console.log("Database connected");
+})
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
