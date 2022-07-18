3386a09c680c4d827ffcbc4d37e657bfa5d97275
Rahul Shingala <rahulshingala111@gmail.com>
2022-07-19 02:46:57 +0530

    edit file added

--- app.js
+++ app.js
@@ -51,6 +51,12 @@ app.get('/campgrounds/:id', async(req, res) => {
     res.render('campgrounds/show',{campground});
 })
  
+app.get('/campgrounds/:id/edit', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{campground});
+})
+ 
+
 
 
 
