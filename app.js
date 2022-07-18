31b3c6bbe255e2a8c1ecb535d978c75933c403f5
Rahul Shingala <rahulshingala111@gmail.com>
2022-07-19 01:16:11 +0530

    furthur progress

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ app.get('/campgrounds', async(req, res) => {
 })
 
 app.get('/campgrounds/:id', async(req, res) => {
-    res.render('campgrounds/show');
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground});
  })
  
 
